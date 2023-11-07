1c82af8d34c301706125fcc2d5b7bce14cad7937
DimitarAtanassov <dimitar789558@gmail.com>
2023-11-06 18:28:31 -0700

    Updated send_transaction logic

--- API Gateway Services/api_gateway.py
+++ API Gateway Services/api_gateway.py
@@ -8,6 +8,7 @@ from pymongo import MongoClient
 
 #Flask and Vars
 app = Flask(__name__)
+os.environ['GANACHE_URL'] = 'HTTP://127.0.0.1:7545'  # Replace with your desired URL
 ganache_url = os.environ.get('GANACHE_URL')
 account_priv_key = os.environ.get('ACCOUNT_PRIV_KEY')
 public_key = os.environ.get('PUBLIC_KEY')
@@ -51,30 +52,40 @@ def verify_jwt():
 
 @app.route('/send-transaction', methods=['POST'])
 def send_transaction():
+    
+    # Get Transaction info from Front End and send transaction to Ganache
     data = request.json
-    to_address = data.get('to_address')
-    amount_in_wei = data.get('amount_in_wei')
-    gas = data.get('gas')
-    gas_price = data.get('gas_price')
-    account_address = "ENTER ACCOUNT ADDRESS HERE"
-
-    if not (to_address and amount_in_wei and gas and gas_price):
+    
+    #Reterving Data from the json request
+    to_address = data.get('to_address') # Address of the Reciever
+    transaction_amount = data.get('transaction_amount') # Transaction Amount
+    gas = data.get('gas')   # Represents cost of executing operations on the Ethereum Network
+    gas_price = data.get('gas_price') #Amount of Ether a person is willing to pay for each unit of gas used in the transaction
+    from_address = data.get('from_address') # Address of the Sender
+    sender_private_key = data.get('private_key') # Private key of the Sender
+    
+    # Format data in order to send transaction to Ganache 
+    amount_in_wei = w3.to_wei(transaction_amount,'ether') # Assuming the transaction amount will always be in ether when retrieved from Front End
+    gas_price_in_wei = w3.to_wei(gas_price,'gwei') # Assuming gas_price will always be in gwei when retrieved from Front End
+    
+    # Valadating data
+    if not (to_address and amount_in_wei and gas and gas_price_in_wei):
         return jsonify({"error": "Missing required parameters"}), 400
-
+    
+    # Connecting to Ganache
     w3 = Web3(Web3.HTTPProvider(ganache_url))
-    # account_address = os.environ.get('ACCOUNT_ADDRESS')
 
+    #Create transaction object that will be sent to Ganache
     transaction = {
         'to': to_address,
         'value': amount_in_wei,
         'gas': gas,
-        'gasPrice': w3.toWei(gas_price, 'gwei'),
-        'nonce': w3.eth.getTransactionCount(account_address)
+        'gasPrice': gas_price_in_wei,
+        'nonce': w3.eth.getTransactionCount(from_address)
     }
-
-    # ------ transaction logic just in case we needa use it -----------------
-    # signed_txn = w3.eth.account.signTransaction(transaction, account_priv_key)
-    # txn_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
-
-    # return jsonify({"transaction_hash": txn_hash.hex()}), 200
+    signed_transaction = w3.eth.account.sign_transaction(transaction, sender_private_key)
+    transaction_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+ 
+    # Return the Transaction Hash after the transaction is complete
+    return jsonify({"Transaction_Hash" : transaction_hash.hex()}), 200
 
--- API Gateway Services/jwt_generator.py
+++ API Gateway Services/jwt_generator.py
@@ -4,7 +4,7 @@ from web3 import Web3
 
 
 #Connecting to web3 with Ganache 
-w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:5000"))
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
 
 #Header: Algo & Token Type
 #   Alg: Signature / Encryption Algo
@@ -20,8 +20,8 @@ amount_wei = w3.to_wei(amount_eth,'ether')
 gas = 50000
 gas_price_gwei = 20
 gas_price_wei = w3.to_wei(gas_price_gwei,'gwei')
-to_address = "0xae01BcD0bBAa2Bd3B1abE0b910ABe14A0Ea8f85d"
-from_address = "0x7Ee06E3a8FCdFB11D41f8F2872b7327A7fC5fcE5"
+to_address = "0xdf36eFD9a38C7A687B7b660f4537d800806BCb55"
+from_address = "0x14dBc93d7098F2284fd410e344736f50F19C7EAe"
 
 #Payload: Data
 transaction = {
@@ -34,7 +34,7 @@ transaction = {
 
 
 #Private key of sender used to sign the transaction
-private_key = "0xc86fc1439e1deb777e4fc60e54a9192412c3ba3fc2a7f6c86ba2a0aa1115bab1"
+private_key = "0x0778f3fcc88d794d1992d96ea6f870cd2ea330aa8c79c9ab12fb762cebe50152"
 
 # Sign and send the transaction
 signed_transaction = w3.eth.account.sign_transaction(transaction, private_key)
