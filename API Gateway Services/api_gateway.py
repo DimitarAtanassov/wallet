b1fea0e8087154fcf7d79461338ef1e2c691ded6
DimitarAtanassov <dimitar789558@gmail.com>
2023-11-16 17:42:16 -0700

    Connected Fronted and Backend

--- API Gateway Services/api_gateway.py
+++ API Gateway Services/api_gateway.py
@@ -1,41 +1,45 @@
 from flask import Flask, request, jsonify
-import requests
-import jwt
-import hashlib
 from web3 import Web3
 import os
 import config
+from flask_cors import CORS
 
-#Flask and Vars
+# Flask and Vars
 app = Flask(__name__)
-ganache_url = config.GANACHE_URL
+CORS(app)
+ganache_url = "HTTP://127.0.0.1:7545"
 
 
-#For mongoDB 
-"""client = MongoClient("")
+
+# Generate JWT
+# Encode JWT
+# Use verify_jwt() to decode the token and see what the response looks like
+# Start looking into ganache 
+
+# For mongoDB 
+"""
+client = MongoClient("")
 db = client[""]
-wallet_collection = db[""] """
+wallet_collection = db[""]
+"""
 
-"""Status Codes:
-    1xx: Informational
-    2xx: Success (Client request was successful)
-    3xx: Redirection (Clieant must take additional actions in order for their request to be completed)
-    4xx: Client Error (Error at client request)
-    5xx: Server Error (Error at the server side)
-    
-""" 
-#Generate JWT
-#Encode JWT
-#Use verify_jwt() to decode the token and see what the response looks like
-#Start looking into ganache 
+"""
+Status Codes:
+1xx: Informational
+2xx: Success (Client request was successful)
+3xx: Redirection (Client must take additional actions to complete their request)
+4xx: Client Error (Error at client request)
+5xx: Server Error (Error at the server side)
+"""
 
+# Uncomment the following lines if needed
 # @app.route('/verify-jwt', methods=['POST'])
 # def verify_jwt():
 #     data = request.json
 #     jwt_token = data.get('jwt')
 #     if not jwt_token:
 #         return jsonify({"error": "Missing JWT token"}), 400
-
+#
 #     try:
 #         data_payload = jwt.decode(jwt_token, public_key, algorithms=['RS256'])
 #         return jsonify(data_payload), 200
@@ -48,59 +52,61 @@ wallet_collection = db[""] """
 
 @app.route('/send-transaction', methods=['POST'])
 def send_transaction():
-    
     # Get Transaction info from Front End and send transaction to Ganache
     data = request.json
+    if data is None:
+        return jsonify({"error": "Invalid or empty JSON in the request body"}), 400
     
-    #Reterving Data from the json request
-    to_address = data.get('to_address') # Address of the Reciever
-    transaction_amount = data.get('transaction_amount') # Transaction Amount
-    gas = data.get('gas')   # Represents cost of executing operations on the Ethereum Network
-    gas_price = data.get('gas_price') #Amount of Ether a person is willing to pay for each unit of gas used in the transaction
-    from_address = data.get('from_address') # Address of the Sender
-    sender_private_key = data.get('private_key') # Private key of the Sender
-    
-    # Format data in order to send transaction to Ganache 
-    amount_in_wei = w3.to_wei(transaction_amount,'ether') # Assuming the transaction amount will always be in ether when retrieved from Front End
-    gas_price_in_wei = w3.to_wei(gas_price,'gwei') # Assuming gas_price will always be in gwei when retrieved from Front End
-    
-    # Valadating data
-    if(
-        not all([to_address,transaction_amount,gas,gas_price,from_address,sender_private_key]) or
-        not isinstance(to_address,str) or
-        not isinstance(transaction_amount, (int,float)) or
-        not isinstance(gas,int) or
-        not isinstance(gas_price,int) or 
-        not isinstance(from_address,str) or
-        not isinstance(sender_private_key,str)
-        # Wait for update on if there is a minium treshold price in order to use app
-        # not 0 <= transaction_amount <= Threshold?
-    ):
-        return jsonify({"error" : "Parameters are missing or invalid"}),400
-    
+    to_address = "0x0d6079727FcD9eBbd5D4a74846fbD5Bd2b02055B"
+    # Retrieving Data from the json request
+    transaction_amount = data.get('transaction_amount')  # Transaction Amount
+    gas = data.get('gas')  # Represents cost of executing operations on the Ethereum Network
+    gas_price = data.get('gas_price')  # Amount of Ether a person is willing to pay for each unit of gas used in the transaction
+    from_address = data.get('from_address')  # Address of the Sender
+    sender_private_key = data.get('sender_private_key')  # Private key of the Sender
     # Connecting to Ganache
     try:
         w3 = Web3(Web3.HTTPProvider(ganache_url))
-    except request.exceptions.RequestException:
-        return jsonify({"error" : "Connection to Ganache failed"}),500
+        print("Connection to Blockchain Successful")
+    except requests.exceptions.RequestException:
+        return jsonify({"error": "Connection to Ganache failed"}), 500
+    # Format data to send transaction to Ganache
+    amount_in_wei = w3.to_wei(transaction_amount, 'ether')
+    gas_price_in_wei = w3.to_wei(gas_price, 'gwei')
+    
+    
+
+    # Validating data
+    if (
+            not all([transaction_amount, gas, gas_price, from_address, sender_private_key]) or
+            not isinstance(transaction_amount, (int, float)) or
+            not isinstance(gas, int) or
+            not isinstance(gas_price, int) or
+            not isinstance(from_address, str) or
+            not isinstance(sender_private_key, str)
+    ):
+        return jsonify({"error": "Parameters are missing or invalid"}), 400
+
+    
     try:
-        #Create transaction object that will be sent to Ganache
+        # Create transaction object that will be sent to Ganache
         transaction = {
             'to': to_address,
+            'from' : from_address,
             'value': amount_in_wei,
             'gas': gas,
             'gasPrice': gas_price_in_wei,
-            'nonce': w3.eth.getTransactionCount(from_address)
+            'nonce': w3.eth.get_transaction_count(from_address)
         }
         signed_transaction = w3.eth.account.sign_transaction(transaction, sender_private_key)
         transaction_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
-    
         # Return the Transaction Hash after the transaction is complete
-        return jsonify({"Transaction_Hash" : transaction_hash.hex()}), 200
+        return jsonify({"Transaction_Hash": transaction_hash.hex()}), 200
     except ValueError as e:
         err_msg = str(e)
         if "insufficient funds for gas * price + value" in err_msg:
-            return jsonify({"error" : "Insufficient funds for transaction"}), 400
-    
-    
-    
+            return jsonify({"error": "Insufficient funds for transaction"}), 400
+
+# Run the Flask app
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
--- API Gateway Services/jwt_generator.py
+++ API Gateway Services/jwt_generator.py
@@ -1,44 +1,73 @@
 #Generate a JWT token
 import jwt
-from web3 import Web3
+from web3 import Web3,Account
 
 
 #Connecting to web3 with Ganache 
+to_address = "0xae01BcD0bBAa2Bd3B1abE0b910ABe14A0Ea8f85d" # Davids Wallet
+
+from_address = "0x0d6079727FcD9eBbd5D4a74846fbD5Bd2b02055B" # User wallet address
+private_key = "0x8c58cb12c008afc88bf76d224977b1dbdee2d96f70cd908e1510eb0cf9b528a3"
 w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
-
-#Header: Algo & Token Type
-#   Alg: Signature / Encryption Algo
-#   Typ: Type of token
-jwt_header = {
-    "alg":"RS256",
-    "typ": "JWT"
-}
-
-# Values to be used in Payload(Transaction)
-amount_eth = 200 
+amount_eth = 10 
 amount_wei = w3.to_wei(amount_eth,'ether')
 gas = 6721975
 gas_price_gwei = 20
 gas_price_wei = w3.to_wei(gas_price_gwei,'gwei')
-to_address = "0x0a39E65631Df544256EE3f06833De5915181af5F"
-from_address = "0xed30e1dF671Fa865a73d94fA251529CD8d7BFC23"
 
-#Payload: Data
+
+
+
+# Get accounts
+accounts_in_ganache = w3.eth._accounts()
+
+# Create new account for the user sending money with their private key 
+user_balance_before = w3.eth.get_balance(from_address)
+ganache_balance_before = w3.eth.get_balance(to_address)
+
+
+#Balances before transaction
+print(f"Ganache Bal B4: {ganache_balance_before}")
+print(f"New Account Bal B4: {user_balance_before}")
 transaction = {
-    'to': to_address,
-    'value': amount_wei,
-    'gas': gas,
+    'to' : to_address,
+    'from' : from_address,
+    'value' : w3.to_wei(5,'ether'),
+    'gas' : 6721975,
     'gasPrice': gas_price_wei,
     'nonce': w3.eth.get_transaction_count(from_address)
 }
+signed_transaction = w3.eth.account.sign_transaction(transaction, private_key)
+transaction_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+print(f"Hash: {transaction_hash}")
+ganache_bal_after = w3.eth.get_balance(to_address)
+print(f"Ganache Bal After: {ganache_bal_after}")
+# Values to be used in Payload(Transaction)
+
+
+
+#Payload: Data
+# transaction = {
+#     'to': to_address,
+#     'value': amount_wei,
+#     'gas': gas,
+#     'gasPrice': gas_price_wei,
+#     'nonce': w3.eth.get_transaction_count(from_address)
+# }
 
 
 #Private key of sender used to sign the transaction
-private_key = "0x01a8f5ad7784d9d4f191ee9a469a80695ae26b70c4e533c713df70244d9407d0"
+#private_key = "0xc86fc1439e1deb777e4fc60e54a9192412c3ba3fc2a7f6c86ba2a0aa1115bab1"
 
 # Sign and send the transaction
-signed_transaction = w3.eth.account.sign_transaction(transaction, private_key)
-transaction_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
+# signed_transaction = w3.eth.account.sign_transaction(transaction, private_key)
+# print(signed_transaction.rawTransaction)
+# transaction_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
 
 #Transaction hash info
 print(f"Transaction Hash: {transaction_hash.hex()}")
+
+
+    
+
+
