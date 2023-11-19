61ea1bb2b7454e55de261481edcd07d8b7c25736
Wermutton <manimateus99@gmail.com>
2023-11-18 17:24:15 -0700

    feat: adjusted test cases

--- API Gateway Services/tests/test_gateway.py
+++ API Gateway Services/tests/test_gateway.py
@@ -7,28 +7,46 @@ class apiGateWayTests(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
-    
-    # 400 -> Bad Request (Cause of Bad Request is missing params)
-    def test_send_transaction_missing_parameters(self):
+
+    def test_send_transaction_empty_json(self):
         response = self.app.post('/send-transaction', json={})
         self.assertEqual(response.status_code, 400)
         response_data = json.loads(response.data.decode('utf-8'))
-        self.assertEqual({}, {"error": "Parameters are missing or invalid"})
+        self.assertEqual(response_data, {"error": "Invalid or empty JSON in the request body"})
+    
+    # 400 -> Bad Request (Cause of Bad Request is missing params)
+    def test_send_transaction_missing_parameters(self):
+        # Testing with one key missing at a time
+        base_data = {
+            'transaction_amount': 1,
+            'gas': 21000,
+            'gas_price': 1000000000,
+            'from_address': '0xSenderAddress',
+            'sender_private_key': 'valid_private_key',
+        }
+        for key in base_data.keys():
+            data = base_data.copy()
+            del data[key]
+            response = self.app.post('/send-transaction', json=data)
+            self.assertEqual(response.status_code, 400)
+            response_data = json.loads(response.data.decode('utf-8'))
+            self.assertEqual(response_data, {"error": "Parameters are missing or invalid"})
     
     # Mimics a successful transaction
     def test_send_transaction_success(self):
         data = {
-            'to_address': '0xRecipientAddress',
             'transaction_amount': '1',  # 1 Ether
-            'gas': 21000,  # A typical gas amount for simple transfers
-            'gas_price': 1000000000,  # 1 Gwei
+            'gas': 21000,
+            'gas_price': 1000000000,
             'from_address': '0xSenderAddress',
-            'private_key': 'valid_private_key',
+            'sender_private_key': 'valid_private_key',
         }
         response = self.app.post('/send-transaction', json=data)
-        data = json.loads(response.data.decode('utf-8'))
+        response_data = json.loads(response.data.decode('utf-8'))
         self.assertEqual(response.status_code, 200)
-        self.assertIn("Transaction_Hash", data)
+        self.assertIn("Transaction_Hash", response_data)
+        self.assertTrue(response_data.get("Transaction_Status"))  # Check if Transaction_Status is True
+
 
 
     # Test for invalid parameter types for each parameter
@@ -65,19 +83,19 @@ class apiGateWayTests(unittest.TestCase):
 
     # Testing to see if insufficient funds for transaction will be denied
     def test_send_transaction_insufficient_funds(self):
-        
         data = {
-            'to_address': '0xRecipientAddress',
-            'transaction_amount': 100000000000000000000000000,  # 1 Ether
-            'gas': 21000,  # A typical gas amount for simple transfers
-            'gas_price': 1000000000,  # 1 Gwei
+            'transaction_amount': 100000000000000000000000000,  # Extremely high amount
+            'gas': 21000,
+            'gas_price': 1000000000,
             'from_address': '0xSenderAddress',
-            'private_key': 'valid_private_key',
+            'sender_private_key': 'valid_private_key',
         }
         response = self.app.post("/send-transaction", json=data)
-        self.assertEqual(response.status_code,400)
-        data = json.loads(response.data.decde('utf-8'))
-        self.assertEqual(data,{"error" : "Insufficient funds for transaction"})
+        response_data = json.loads(response.data.decode('utf-8'))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response_data, {"error": "Insufficient funds for transaction", "Transaction_Status": False})
+        self.assertFalse(response_data.get("Transaction_Status"))  # Check if Transaction_Status is False
+
     
     # Testing to see if ganache connection failure will be processed
     def test_send_transaction_ganache_connection_failure(self):
